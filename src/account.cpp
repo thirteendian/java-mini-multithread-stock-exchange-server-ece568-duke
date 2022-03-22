7d057732907db68cc8503b551ada0646aa6057f7
Yuxuan Yang <yy340@duke.edu>
2022-03-22 18:31:22 -0400

    Empty Main

--- src/account.cpp
+++
@@ -1,7 +0,0 @@
-#include "account.h"
-
-bool Account::operator==(const Account& rhs) const{
-  return this->accountNumber == rhs.accountNumber &&
-    this->balance == rhs.balance &&
-    this->positions == rhs.positions;
-}
--- src/symbol.cpp
+++
@@ -1,9 +0,0 @@
-#include "symbol.h"
-
-bool Symbol::operator==(const Symbol& rhs) const{
-  return this->identifier == rhs.identifier;
-}
-
-std::string& Symbol::get_identifier(){
-  return this->identifier;
-}
