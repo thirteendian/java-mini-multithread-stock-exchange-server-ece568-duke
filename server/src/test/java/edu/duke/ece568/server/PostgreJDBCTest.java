5be324fde879d8fa917e3a03d2efb859d8cdfa05
Anran Chen <anran.chen013@duke.edu>
2022-03-23 15:46:15 -0400

    feature update: finished table creation during initialization

--- server/src/test/java/edu/duke/ece568/server/PostgreJDBCTest.java
+++ server/src/test/java/edu/duke/ece568/server/PostgreJDBCTest.java
@@ -9,10 +9,14 @@ import org.junit.jupiter.api.Test;
 
 public class PostgreJDBCTest {
 
+    private PostgreJDBC helper_generateValidJdbc() throws ClassNotFoundException, SQLException{
+        return new PostgreJDBC("localhost", "5432", "ece568_hw4", "postgres", "passw0rd");
+    }
+
     @Test
     public void test_constructor() throws ClassNotFoundException, SQLException{
         // correct
-        assertDoesNotThrow(()->new PostgreJDBC("localhost", "5432", "ece568_hw4", "postgres", "passw0rd"));
+        assertDoesNotThrow(()->this.helper_generateValidJdbc());
         // wrong host
         assertThrows(SQLException.class, () -> new PostgreJDBC("localhostxxx", "5432", "ece568_hw4", "postgres", "passw0rd"));
         // wrong port
@@ -23,5 +27,16 @@ public class PostgreJDBCTest {
         assertThrows(SQLException.class, () -> new PostgreJDBC("localhost", "5432", "ece568_hw3", "postgresXXXX", "passw0rd"));
         // wrong password
         assertThrows(SQLException.class, () -> new PostgreJDBC("localhost", "5432", "ece568_hw3", "postgres", "asdf"));
+    }
+
+    @Test
+    public void test_createTablesIfNotExist() throws ClassNotFoundException, SQLException{
+        PostgreJDBC jdbc = this.helper_generateValidJdbc();
+
+        // if empty, test creation
+        // test creation when existing
+        for(int i = 0; i < 2; i++){
+            assertDoesNotThrow(()->jdbc.createTablesIfNotExist());
+        } 
     }
 }
