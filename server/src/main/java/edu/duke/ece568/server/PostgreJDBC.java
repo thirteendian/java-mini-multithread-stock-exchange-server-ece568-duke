6cb80da35ad66781243b5dd1dcc96c466a9376ab
Anran Chen <anran.chen013@duke.edu>
2022-03-23 21:59:51 -0400

    feature update: added API for Account and Position

--- server/src/main/java/edu/duke/ece568/server/PostgreJDBC.java
+++ server/src/main/java/edu/duke/ece568/server/PostgreJDBC.java
@@ -2,6 +2,7 @@ package edu.duke.ece568.server;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -28,19 +29,42 @@ public class PostgreJDBC {
         );
     }
 
-    protected void createTablesIfNotExist() throws SQLException{
-        this.createAccountTable();
-        this.createPositionTable();
-        this.createOrderTable();
+    protected boolean createTablesIfNotExist() throws SQLException{
+        int errorCounter = 0;
+
+        errorCounter += this.createAccountTable() ? 0:1;
+        errorCounter += this.createPositionTable() ? 0:1;
+        errorCounter += this.createOrderTable() ? 0:1;
+
+        return errorCounter > 0 ? false: true;
     }
 
-    protected void executeStatement(String query) throws SQLException{
-        Statement statement = this.conn.createStatement();
-        statement.executeUpdate(query);
-        statement.close();
+    protected boolean executeUpdateStatement(String query){
+        try{
+            Statement statement = this.conn.createStatement();
+            statement.executeUpdate(query);
+            statement.close();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 
-    protected void createAccountTable() throws SQLException{
+    protected ResultSet executeQueryStatement(String query){
+        try{
+            Statement statement = this.conn.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            return resultSet;
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    protected boolean createAccountTable(){
         String query = 
             "CREATE TABLE IF NOT EXISTS ACCOUNT(" +
                 "ACCOUNT_ID SERIAL PRIMARY KEY," + 
@@ -48,16 +72,18 @@ public class PostgreJDBC {
                 "BALANCE FLOAT NOT NULL CHECK (BALANCE >= 0)" + 
             ");";
 
-        this.executeStatement(query);
+        return this.executeUpdateStatement(query);
     }
 
-    protected void createPositionTable() throws SQLException{
+    protected boolean createPositionTable(){
         String query = 
             "CREATE TABLE IF NOT EXISTS POSITION("+
                 "POSITION_ID SERIAL PRIMARY KEY," + 
                 "ACCOUNT_NUMBER INT NOT NULL CHECK (ACCOUNT_NUMBER >= 0)," + 
                 "SYMBOL VARCHAR (255) NOT NULL," + 
                 "AMOUNT FLOAT NOT NULL CHECK (AMOUNT > 0)," + 
+
+                "UNIQUE (ACCOUNT_NUMBER, SYMBOL)," + 
 
                 "CONSTRAINT FK_ACCOUNT " + 
                     "FOREIGN KEY (ACCOUNT_NUMBER) " + 
@@ -66,10 +92,12 @@ public class PostgreJDBC {
                     "ON DELETE SET NULL " + 
             ");";
         
-        this.executeStatement(query);
+        return this.executeUpdateStatement(query);
     }
 
-    protected void createOrderTable() throws SQLException{
+    protected boolean createOrderTable(){
+        int errorCounter = 0;
+
         String enumQuery = 
             "DO $$ BEGIN "+
                 "CREATE TYPE STATUS AS ENUM ('OPEN', 'EXECUTED', 'CANCELLED');"+
@@ -92,27 +120,107 @@ public class PostgreJDBC {
                     "ON DELETE SET NULL " + 
             ");";
 
-        this.executeStatement(enumQuery);
-        this.executeStatement(tableQuery);
+        errorCounter += this.executeUpdateStatement(enumQuery) ? 0 : 1;
+        errorCounter += this.executeUpdateStatement(tableQuery) ? 0 : 1;
+
+        return errorCounter > 0 ? false: true;
     }
 
     /**
-     * create an account, return true upon success, false otherwise
+     * create an account
      * @param accountId new account ID
      * @param balance new balance 
      * @return true upon success, false otherwise
      */
-    public boolean tryCreateAccount(int accountId, double balance){
+    public boolean tryCreateAccount(int accountNumber, double balance){
+        String query = 
+            "INSERT INTO ACCOUNT(ACCOUNT_NUMBER, BALANCE) " + 
+            "VALUES (" + accountNumber + ", " + Double.toString(balance) + ");";
+
+        return this.executeUpdateStatement(query);
+    }
+
+    /**
+     * for a given account, create a position if not exist
+     * update the amount if exist
+     * if the updated amount = 0, delete entry from table
+     * @param accountNumber existing account number
+     * @param symbol new symbol
+     * @param amount new amount
+     * @return true upon success, false otherwise
+     * @throws SQLException
+     */
+    public boolean tryUpdateOrCreatePosition(int accountNumber, String symbol, double amount) throws SQLException{
         try{
-            String query = 
-                "INSERT INTO ACCOUNT(ACCOUNT_NUMBER, BALANCE) " + 
-                "VALUES (" + accountId + ", " + Double.toString(balance) + ");";
-            this.executeStatement(query);
+            this.conn.setAutoCommit(false);
+            boolean isTransactionSuccessful = this.tryUpdateOrCreatePositionTransaction(accountNumber, symbol, amount);
+            this.conn.commit();
+            return isTransactionSuccessful;
+        }
+        catch(SQLException e){
+            this.conn.rollback();
+            return false;
+        }  
+    }
+
+    /**
+     * transaction implementation for wrapper tryUpdateOrCreatePosition
+     * @param accountNumber existing account number
+     * @param symbol new symbol
+     * @param amount new amount
+     * @return true upon success, false otherwise
+     * @throws SQLException
+     */
+    protected boolean tryUpdateOrCreatePositionTransaction(int accountNumber, String symbol, double amount) throws SQLException{
+        try{
+            if(amount == 0){ // cannot update with an offset of 0
+                return false;
+            }
+            else if(amount < 0){ // if offset < 0
+                // getting current amount
+                ResultSet resultSet = this.executeQueryStatement(
+                    "SELECT AMOUNT FROM POSITION " +
+                    "WHERE ACCOUNT_NUMBER=" + accountNumber + " " + 
+                    "AND SYMBOL=\'" + symbol + "\' FOR UPDATE;"
+                );
+                // if current amount exist when trying to decrease, error
+                if(resultSet == null || !resultSet.next()){ 
+                    return false;
+                }
+                else{
+                    double amountFromDb = resultSet.getDouble("AMOUNT");
+                    // if current amount = amount want to remove, delete row
+                    if(-1*amount == amountFromDb){ 
+                        return this.executeUpdateStatement(
+                            "DELETE FROM POSITION " + 
+                            "WHERE ACCOUNT_NUMBER=" + accountNumber + " " +
+                            "AND SYMBOL=\'" + symbol + "\';"
+                        );
+                    }
+                    else{ // if current amount != amount want to remove, update value
+                        return this.executeUpdateStatement( 
+                            "UPDATE POSITION SET AMOUNT=AMOUNT+" + amount + " " + 
+                            "WHERE ACCOUNT_NUMBER=" + accountNumber + " " + 
+                            "AND SYMBOL=\'" + symbol + "\';"
+                        );
+                    }
+                }
+            }
+            else{ // if offset > 0, create or update amount
+                String query = 
+                "INSERT INTO POSITION(ACCOUNT_NUMBER, SYMBOL, AMOUNT) " + 
+                "VALUES (" + accountNumber  + ", \'" + symbol + "\', " + amount + ") " +
+                "ON CONFLICT (ACCOUNT_NUMBER, SYMBOL) DO " + 
+                "UPDATE SET AMOUNT=POSITION.AMOUNT+" + amount + " " + 
+                "WHERE EXCLUDED.ACCOUNT_NUMBER=" + accountNumber + " " + 
+                "AND EXCLUDED.SYMBOL=\'" + symbol + "\';";
+
+                return this.executeUpdateStatement(query);
+            }
         }
         catch(SQLException e){
             e.printStackTrace();
             return false;
         }
-        return true;
     }
 }
