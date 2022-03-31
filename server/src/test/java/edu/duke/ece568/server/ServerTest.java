fc36a8493984e3c8f839153b53166b5fac496363
Yuxuan Yang <yy340@duke.edu>
2022-03-31 17:42:49 -0400

    Server Test

--- server/src/test/java/edu/duke/ece568/server/ServerTest.java
+++ server/src/test/java/edu/duke/ece568/server/ServerTest.java
@@ -4,6 +4,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class ServerTest {
-    
+    @Test
+    public void test_ServerTest() throws IOException {
+        Server myserver = new Server();
+        MockClient mockClient = new MockClient(12345, "127.0.0.1");
+        Socket clientSocket = myserver.acceptConnection();
+        ServerRunnable serverRunnable = new ServerRunnable(clientSocket);
+        Thread t = new Thread(serverRunnable);
+        t.start();
+
+
+        String XML_str = Files.readString(Path.of("/Users/yuxuan/Project/erss-hwk4-ac692-yy340/SampleXML/ARequest.xml"));
+        XML_str = "172\n"+XML_str;
+        mockClient.sendMsg(XML_str);
+        String recvMsg;
+        recvMsg = mockClient.recvMsg();
+        System.out.println(recvMsg);
+    }
+
 }
